a5977834234b614f4da6b689ca4566f52be27e7c
Fernandez Santiago <santiago@MBPdeFernandez.fibertel.com.ar>
2020-07-31 08:48:28 -0300

    Scripts

---
+++ importToDefectDojo.py
@@ -0,0 +1,23 @@
+import requests
+import json
+from datetime import datetime
+
+url = 'http://XX.XX.XX.XX:8080/api/v2/import-scan/'
+
+headers = {'Authorization': 'Token XXXXXXXXXXXXX'}
+
+files = {'file': open('/Users/santiago/Desktop/dependency-check-report.xml','rb')}
+
+dia = datetime.today().strftime('%Y-%m-%d')
+print(dia)
+
+body = {'scan_date': dia,
+'scan_type': 'Dependency Check Scan',
+'engagement': '1',
+'lead': '1'
+}
+
+r = requests.post(url, headers=headers, files=files, verify=False, data=body)
+
+print("Status Code :%s"%r.status_code)
+print("Respuesta :%s"%r.text)
